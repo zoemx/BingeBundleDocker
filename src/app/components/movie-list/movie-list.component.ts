4b6bb5035edb95087ed793a59ecf05436cc3669a
Arwa Sharif <arwas@google.com>
2024-05-03 09:44:43 -0500

    adding movie titles for the home page

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -21,7 +21,7 @@ export class MovieListComponent {
   ngOnInit():void{
     //TODO: change this api method to what Lavon and Zoe created last week. 
     //Shami- sorry I needed to change this for a merge conflict
-    this.MoveApiService.getHomepageTVTitles().subscribe((resp:any) => {
+    this.MoveApiService.getHomepageMovieTitles().subscribe((resp:any) => {
       console.log(resp);
       resp.results.forEach((movie: any) => {
         this.movie_titles.push(movie);
--- src/app/services/move-api-service.ts
+++ src/app/services/move-api-service.ts
@@ -20,6 +20,11 @@ export class MoveApiService {
     return this.HttpClient.get<TVTitle[]>('https://api.themoviedb.org/3/tv/popular', {headers})
    } 
 
+   getHomepageMovieTitles(){
+    const headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTExYTBhZDNjN2Y3NGE1ODY3MThkODRlYzdjMTNlNiIsInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEGEsfW6lpxXIlOoiGs616XICs0SfBIZlBSXvvVAR1w'}
+    return this.HttpClient.get<MovieTitle[]>('https://api.themoviedb.org/3/movie/popular', {headers})
+   }
+
    //TODO: REDO this 
    getSingleMovieTitle(){
     const headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTExYTBhZDNjN2Y3NGE1ODY3MThkODRlYzdjMTNlNiIsInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEGEsfW6lpxXIlOoiGs616XICs0SfBIZlBSXvvVAR1w'}

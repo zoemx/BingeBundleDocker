15207832ab657faccadadd8bd742c9f72f905816
Arwa Sharif <arwas@google.com>
2024-05-09 13:19:35 -0500

    fix header component failing test

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,19 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import for animations
 import { HeaderComponent } from './header.component';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { MovieApiService } from '../../services/tmdb-api-service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let movieApiService: MovieApiService;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent, BrowserAnimationsModule],
+      imports: [HeaderComponent, HttpClientTestingModule, BrowserAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    // Inject the http, test controller, and service-under-test
+    // as they will be referenced by each test.
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    movieApiService = TestBed.inject(MovieApiService);
   });
 
   it('should create', () => {

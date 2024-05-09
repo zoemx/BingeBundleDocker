243f0d4064f3c3baf664328cd842eedfda86c91a
Arwa Sharif <arwas@google.com>
2024-05-09 15:48:06 -0500

    cleanup & add tests for getMovieTitles, getSingleMovieTitle, and getSearch

--- src/app/services/tmdb-api-service.spec.ts
+++ src/app/services/tmdb-api-service.spec.ts
@@ -6,7 +6,11 @@ import {
   HttpTestingController,
   HttpClientTestingModule,
 } from '@angular/common/http/testing';
-import { TVTitle } from '../../interfaces/streaming-Service';
+import {
+  MovieTitle,
+  SearchTitles,
+  TVTitle,
+} from '../../interfaces/streaming-Service';
 
 describe('TMDB-API-Service', () => {
   let httpClient: HttpClient;
@@ -28,9 +32,8 @@ describe('TMDB-API-Service', () => {
 
   describe('#getTvTitles', async () => {
     let expectedTvTitles: TVTitle[];
-
+    let pageNumber: number = 1;
     beforeEach(() => {
-      //movieApiService = TestBed.inject(MovieApiService);
       expectedTvTitles = [
         { id: 1, name: 'A', poster_path: '/poster' },
         { id: 2, name: 'B', poster_path: '/poster' },
@@ -43,7 +46,7 @@ describe('TMDB-API-Service', () => {
     });
 
     it('should return expected tv titles (called once)', async () => {
-      movieApiService.getHomepageTVTitles(1).subscribe({
+      movieApiService.getHomepageTVTitles(pageNumber).subscribe({
         next: (tvTitles) =>
           expect(tvTitles).toEqual(
             expectedTvTitles,
@@ -54,7 +57,7 @@ describe('TMDB-API-Service', () => {
 
       // Expect one request to the API with the correct URL and parameters
       const req = httpTestingController.expectOne(
-        'https://api.themoviedb.org/3/tv/popular?page=1&limit=100' // Assuming these are the expected parameters
+        `https://api.themoviedb.org/3/tv/popular?page=${pageNumber}&limit=100` // Assuming these are the expected parameters
       );
       expect(req.request.method).toEqual('GET');
 
@@ -125,40 +128,110 @@ describe('TMDB-API-Service', () => {
     //     requests[2].flush(expectedTvTitles);
     //   });
   });
+
+  describe('#getMovieTitles', async () => {
+    let expectedMovieTitles: MovieTitle[];
+    let pageNumber: number = 1;
+
+    beforeEach(() => {
+      expectedMovieTitles = [
+        { id: 1, title: 'A', poster_path: '/poster' },
+        { id: 2, title: 'B', poster_path: '/poster' },
+      ] as MovieTitle[];
+    });
+
+    it('should return expected movie titles (called once)', async () => {
+      movieApiService.getHomepageMovieTitles(pageNumber).subscribe({
+        next: (movieTitles) =>
+          expect(movieTitles).toEqual(
+            expectedMovieTitles,
+            'should return expected movie titles'
+          ),
+        error: fail,
+      });
+
+      // Expect one request to the API with the correct URL and parameters
+      const req = httpTestingController.expectOne(
+        `https://api.themoviedb.org/3/movie/popular?page=${pageNumber}&limit=100` // Assuming these are the expected parameters
+      );
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock data (assuming expectOne doesn't automatically trigger response)
+      req.flush(expectedMovieTitles);
+
+      // Verify no outstanding requests after test completes
+      httpTestingController.verify();
+    });
+  });
+
+  describe('#getSingleMovieTitle', async () => {
+    let expectedMovieTitle: MovieTitle;
+
+    beforeEach(() => {
+      expectedMovieTitle = {
+        id: 1,
+        title: 'A',
+        poster_path: '/poster',
+      } as MovieTitle;
+    });
+
+
+    it('should return expected movie title (called once)', async () => {
+      movieApiService.getSingleMovieTitle().subscribe({
+        next: (movieTitle) =>
+          expect(movieTitle).toEqual(
+            expectedMovieTitle,
+            'should return expected tv titles'
+          ),
+        error: fail,
+      });
+
+      // Expect one request to the API with the correct URL and parameters
+      const req = httpTestingController.expectOne(
+        'https://api.themoviedb.org/3/movie/948549?language=en-US' // Assuming these are the expected parameters
+      );
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock data (assuming expectOne doesn't automatically trigger response)
+      req.flush(expectedMovieTitle);
+
+      // Verify no outstanding requests after test completes
+      httpTestingController.verify();
+    });
+  });
+
+  describe('#getSearch', async () => {
+    let expectedSearchTitles: SearchTitles[];
+    let searchText: string = 'The Office'
+
+    beforeEach(() => {
+      expectedSearchTitles = [
+        { id: 1, title: 'A', poster_path: '/poster' },
+        { id: 2, name: 'B', poster_path: '/poster' },
+      ] as SearchTitles[];
+    });
+
+    it('should return expected movie title (called once)', async () => {
+      movieApiService.getSearch(searchText).subscribe({
+        next: (searchResults) =>
+          expect(searchResults).toEqual(
+            expectedSearchTitles,
+            'should return expected tv titles'
+          ),
+        error: fail,
+      });
+
+      // Expect one request to the API with the correct URL and parameters
+      const req = httpTestingController.expectOne(
+        `https://api.themoviedb.org/3/search/multi?query=${searchText}&include_adult=false&language=en-US&page=1` // Assuming these are the expected parameters
+      );
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock data (assuming expectOne doesn't automatically trigger response)
+      req.flush(expectedSearchTitles);
+
+      // Verify no outstanding requests after test completes
+      httpTestingController.verify();
+    });
+  });
 });
-
-/* let component: TvListComponent;
-let fixture: ComponentFixture<TvListComponent>;
-
-
-
-beforeEach(async () => {
-  await TestBed.configureTestingModule({
-    imports: [TvListComponent,
-    HttpClientTestingModule
-    ],
-  })//.compileComponents();
- */
-//separating these two does not change hasOwnProperty error
-/* TestBed.configureTestingModule({
-    // Import the HttpClient mocking services
-    imports: [HttpClientTestingModule],
-    // Provide the service-under-test and its dependencies
-    // providers: [movieApiService],
-  }); */
-
-/*   fixture = TestBed.createComponent(TvListComponent);
-  component = fixture.componentInstance;
-  fixture.detectChanges(); */
-
-// Inject the http, test controller, and service-under-test
-/*   // as they will be referenced by each test.
-  httpClient = TestBed.inject(HttpClient);
-  httpTestingController = TestBed.inject(HttpTestingController);
-  movieApiService = TestBed.inject(MovieApiService);
-});
- */
-
-/*  it('should create', () => {
-  expect(component).toBeTruthy();
-}); */

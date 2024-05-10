1c69610e881a62bed60f75a7b744ca662c138c4a
shamito <shamito@google.com>
2024-05-10 12:37:20 -0400

    create homepage and details components

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TvListComponent } from '../components/tv-list/tv-list.component';
+import { MovieListComponent } from '../components/movie-list/movie-list.component';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [TvListComponent, MovieListComponent],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}

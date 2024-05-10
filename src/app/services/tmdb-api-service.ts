b39a7e00385b8543bd4dd6b9afeb7009230151ca
Arwa Sharif <arwas@google.com>
2024-05-10 15:58:38 -0500

    connect movie/tv from list to details page and show data

--- src/app/services/tmdb-api-service.ts
+++ src/app/services/tmdb-api-service.ts
@@ -56,4 +56,26 @@ export class MovieApiService {
       { headers }
     );
   }
+
+  getTVDetails(seriesId: number) {
+    const headers = {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTExYTBhZDNjN2Y3NGE1ODY3MThkODRlYzdjMTNlNiIsInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEGEsfW6lpxXIlOoiGs616XICs0SfBIZlBSXvvVAR1w',
+    };
+    return this.httpClient.get<any>(
+      `https://api.themoviedb.org/3/tv/${seriesId}`,
+      { headers }
+    );
+  }
+
+  getMovieDetails(movieId: number) {
+    const headers = {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTExYTBhZDNjN2Y3NGE1ODY3MThkODRlYzdjMTNlNiIsInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEGEsfW6lpxXIlOoiGs616XICs0SfBIZlBSXvvVAR1w',
+    };
+    return this.httpClient.get<any>(
+      `https://api.themoviedb.org/3/movie/${movieId}`,
+      { headers }
+    );
+  }
 }

3e0c8c7a0abd5229eb8210c315789e986bfa8dda
Arwa Sharif <arwas@google.com>
2024-05-18 18:25:26 -0500

    Style and cleanup cart page

--- src/app/components/cart-page/cart-page.component.ts
+++ src/app/components/cart-page/cart-page.component.ts
@@ -1,38 +1,33 @@
-import { Component, Provider } from '@angular/core';
-import { MovieApiService } from '../../services/tmdb-api-service';
+import { Component } from '@angular/core';
 import { CartItems } from '../../../interfaces/cart';
-import { streamingServices } from '../../../streaming_services';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
 import { CartService } from '../../services/cart-service.service';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, MatCardModule, MatIconButton, MatIcon],
+  imports: [CommonModule, NgOptimizedImage, MatCardModule],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css',
 })
 export class CartPageComponent {
-  
   cart: CartItems[] = [];
 
-  bundleTotal : number = 0;
+  bundleTotal: number = 0;
 
-  getBundleTotal(){
-    this.cart.map(item => {
-      this.bundleTotal += item.price;  
-    })
+  getBundleTotal() {
+    this.cart.map((item) => {
+      this.bundleTotal += item.price;
+    });
   }
 
-  constructor(private movieApiService: MovieApiService, private cartService: CartService) {}
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.getBundleTotal();
-    this.cartService.getCart().subscribe(cart=>{
+    this.cartService.getCart().subscribe((cart) => {
       this.cart = cart;
-    })
+    });
+    this.getBundleTotal();
   }
 }
